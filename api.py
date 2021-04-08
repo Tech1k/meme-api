fdbecb0c6c299e1411c423a6840500f1e1c4f689
Tech1k <kristian@beyondcoin.io>
2021-04-08 05:40:12 -0600

    Fix formatting

--- api.py
+++ api.py
@@ -12,50 +12,51 @@ TOPICS = [
 ]
 
 def ScrapMemes(topic=0, num=1):
-  try:
-    if num <= 0:
-      return {
-        "code": 400,
-        "message": "Enter a positive Number less than 200"
-      }
-      result = {}
-      num = num % 200
-      if topic == 0:
-        topic = choice(TOPICS)
-      if num == 1:
-        subreddit = reddit.subreddit(topic)
-        meme = subreddit.random()
-        try:
-          _ = meme.preview
-          result = {
-            "code":200,
-            "post_link": meme.shortlink,
-            "subreddit": topic,
-            "title": meme.title,
-            "url": meme.url,
-            "ups": meme.ups,
-            "author": meme.author.name,
-            "spoilers_enabled": subreddit.spoilers_enabled,
-            "nsfw": subreddit.over18,
-            "image_previews": [i["url"] for i in meme.preview.get("images")[0].get("resolutions")]
-          }
-        except Exception as e:
-          result = {
-            "post_link": meme.shortlink,
-            "subreddit": topic,
-            "title": meme.title,
-            "url": meme.url,
-            "ups": meme.ups,
-            "author": meme.author.name,
-            "spoilers_enabled": subreddit.spoilers_enabled,
-            "nsfw": subreddit.over18,
-            "image_previews": ["No Preview Found For This Meme.. Sorry For That"]
-          }
-      return result
+    try:
+        if num <= 0:
+            return {
+                "code": 400,
+                "message": "Enter a positive Number less than 200"
+            }
+        result = {}
+        num = num % 200
+        if topic == 0:
+            topic = choice(TOPICS)
+        if num == 1:
+            subreddit = reddit.subreddit(topic)
+            meme = subreddit.random()
+            try:
+                _ = meme.preview
+                result = {
+                    "code":200,
+                    "post_link": meme.shortlink,
+                    "subreddit": topic,
+                    "title": meme.title,
+                    "url": meme.url,
+                    "ups": meme.ups,
+                    "author": meme.author.name,
+                    "spoilers_enabled": subreddit.spoilers_enabled,
+                    "nsfw": subreddit.over18,
+                    "image_previews": [i["url"] for i in meme.preview.get("images")[0].get("resolutions")]
+                }
+            except Exception as e:
+                result = {
 
-  except Exception as e:
-    return {
-      "code": 400,
-      "message": str(type(e))+str(e),
-      "help": "Subreddit Doesn't Exist, Check if u spelled it correctly.."
-    }
+                    "post_link": meme.shortlink,
+                    "subreddit": topic,
+                    "title": meme.title,
+                    "url": meme.url,
+                    "ups": meme.ups,
+                    "author": meme.author.name,
+                    "spoilers_enabled": subreddit.spoilers_enabled,
+                    "nsfw": subreddit.over18,
+                    "image_previews": ["No Preview Found For This Meme.. Sorry For That"]
+                }
+        return result
+
+    except Exception as e:
+        return {
+            "code": 400,
+            "message": str(type(e))+str(e),
+            "help": "Subreddit Doesn't Exist, Check if u spelled it correctly.."
+        }
